5b6172439b6c021133d46b77664eed24092bbd11
cmhug <cmhug@LAPTOP-AR07F35P>
2019-02-20 19:53:06 -0600

    In Class

--- Random Name/Program.cs
+++ Random Name/Program.cs
@@ -6,7 +6,33 @@ namespace Random_Name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string[] names = new string[30];
+            int numberOfNames = 30;
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("Give me a name, please.");
+                string name = Console.ReadLine(); 
+                //if(name == "") 
+                if (String.IsNullOrEmpty(name))
+                {
+                    numberOfNames = i;
+                    break;
+                }
+                names[i] = name;                
+            }
+
+            int randomInt = new Random().Next(0, numberOfNames - 1);
+            string nameWinner = names[randomInt];
+            Console.WriteLine($"The winner is: {nameWinner}");
+           
+            //then list all the other names that didn't win
+
+
+            Console.ReadLine();
+            
+
+
         }
     }
 }
