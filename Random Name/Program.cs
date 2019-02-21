a9fe575f9049f3563368c5ab0efb7128b3bff7f7
cmhug <cmhug@LAPTOP-AR07F35P>
2019-02-20 18:14:28 -0600

    Add project files.

---
+++ Random Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
